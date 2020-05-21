916339534d0e10ff53e2d06292e738ac035adbd7
ota-renato <30956931+ota-renato@users.noreply.github.com>
2020-05-21 12:15:55 -0300

    21/05/2020 - Teste unitário do serviço de cliente

--- kz-angular2-webpack-template/src/app/cliente/shared/cliente.service.spec.ts
+++ kz-angular2-webpack-template/src/app/cliente/shared/cliente.service.spec.ts
@@ -7,18 +7,40 @@
 
 import { TestBed } from '@angular/core/testing';
 
-import { ClienteService } from './cliente.service';
+import { ClienteService, Cliente } from './';
 
-describe('ClienteService', () => {
+describe('Suíte de testes do serviço de cliente', () => {
 
-  let clienteService:ClienteService;
+  let clienteService: ClienteService;
+  let cliente: Cliente;
+  let cliente2: Cliente;
 
   beforeEach(() => {
     clienteService = new ClienteService();
+    cliente = new Cliente(1234567890123, 'Fulano');
+    cliente2 = new Cliente(3210987654321, 'Ciclano');
+
+    delete sessionStorage['clientes'];
   });
 
-  it ('deve criar o ClienteService', () => {
-    //TODO
+  it ('Should create a client', () => {
+    clienteService.cadastrar(cliente);
+    let clienteCad = clienteService.buscarPorId(cliente.id);
+
+    expect(clienteCad).toBeDefined();
+    expect(clienteCad.id).toEqual(cliente.id);
+    expect(clienteCad.nome).toEqual(cliente.nome);
   });
-  
+
+  it ('Should list all clients', () => {
+    clienteService.cadastrar(cliente);
+    clienteService.cadastrar(cliente2);
+
+    let clientes: Cliente[] = clienteService.listarTodos();
+
+    expect(clientes).toBeDefined();
+    expect(clientes.length).toEqual(2);
+    expect(clientes[0].id).toEqual(cliente.id);
+    expect(clientes[1].id).toEqual(cliente2.id);
+  });
 });
